cd67fa8d085de3879ccff1b3b50670272c818d67
Iqbal-revolab <iqbal.fauzi@revolab.ai>
2025-06-26 09:03:07 +0800

    fix route

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_route_table" "main"{
   vpc_id = aws_internet_gateway.main.id 
 }
  route {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "0.0.0.0/16"
   gateway_id = aws_internet_gateway.main.id 
  }
  route {
